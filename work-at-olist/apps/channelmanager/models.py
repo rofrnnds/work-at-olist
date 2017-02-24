bb4eeb7559ffb588c6c1a1f21b8b154fa0bd6eb2
Rodrigo <rodrigo@favurit.com>
2017-02-24 15:54:48 -0300

    Adding the Channel model to the app

--- work-at-olist/apps/channelmanager/models.py
+++ work-at-olist/apps/channelmanager/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Channel(models.Model):
+
+    name = models.CharField(max_length=120, unique=True)
+
+    def __str__(self):
+        self.name
--- work-at-olist/apps/channelmanager/tests.py
+++ work-at-olist/apps/channelmanager/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Channel
 
-# Create your tests here.
+
+class TestChannel(TestCase):
+
+    def setUp(self):
+        self.channels_name = "a testing channel"
+        self.channel = Channel.objects.create(name=self.channels_name)
+
+    def test_can_create_a_channel(self):
+        self.assertIsNotNone(self.channel)
+
+    def test_channel_was_created_with_a_name(self):
+        self.assertEqual(self.channel.name, self.channels_name)
