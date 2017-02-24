2b0080d7bf29c7715537f4de27345a79f1ab1536
Rodrigo <rodrigo@favurit.com>
2017-02-24 15:52:16 -0300

    Added the channelmanager app to INSTALLED_APPS

--- work-at-olist/workatolist/settings.py
+++ work-at-olist/workatolist/settings.py
@@ -21,12 +21,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
+    'apps.channelmanager',
 ]
 
 MIDDLEWARE_CLASSES = [
