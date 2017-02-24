5b08e5029fe57b63f2912a98abf1cf7ecf95addd
Rodrigo <rodrigo@favurit.com>
2017-02-24 17:04:14 -0300

    Refactoring tests

--- work-at-olist/apps/channelmanager/tests.py
+++ work-at-olist/apps/channelmanager/tests.py
@@ -6,10 +6,11 @@ class TestChannel(TestCase):
 
     def setUp(self):
         self.channels_name = "a testing channel"
-        self.channel = Channel.objects.create(name=self.channels_name)
+        self.channel = Channel.objects.create(id=1, name=self.channels_name)
 
-    def test_can_create_a_channel(self):
-        self.assertIsNotNone(self.channel)
+    def test_channel_was_created(self):
+        channel = Channel.objects.get(id=1)
+        self.assertIsNotNone(channel)
 
     def test_channel_was_created_with_a_name(self):
         self.assertEqual(self.channel.name, self.channels_name)
@@ -19,12 +20,13 @@ class TestCategory(TestCase):
 
     def setUp(self):
         self.categorys_name = "a testing category"
-        self.channel = Channel.objects.create(name="a testing channel")
+        self.channel = Channel.objects.create(id=1, name="a testing channel")
         self.category = Category.objects.create(
-            name=self.categorys_name, channel=self.channel)
+            id=1, name=self.categorys_name, channel=self.channel)
 
-    def test_can_create_a_category(self):
-        self.assertIsNotNone(self.category)
+    def test_category_was_created(self):
+        category = Category.objects.get(id=1)
+        self.assertIsNotNone(category)
 
     def test_category_was_created_with_a_name(self):
         self.assertEqual(self.category.name, self.categorys_name)
@@ -33,4 +35,4 @@ class TestCategory(TestCase):
         self.subcategory = Category.objects.create(
             name="a testing sub-category", channel=self.channel,
             parent=self.category)
-        self.assertIsNotNone(self.subcategory)
+        self.assertIsNotNone(self.subcategory.parent)
